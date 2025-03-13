57966a143a8e70a39fa63980cf5454ba5dae14c4
Lorenzo Di Napoli <loridinapoli@gmail.com>
2025-03-13 22:11:59 +0100

    Add socket test_functions

--- lib/exceptions.hpp
+++ lib/exceptions.hpp
@@ -40,16 +40,61 @@ class Socket_Error : public Exception {
 
 
 // prototypes
-void test_socket(int socket);
+void test_socket_creation(int const socket);
+void test_socket_bind(int const bind, int const port);
+void test_socket_listen(int const listen);
+void test_socket_accept(int const socket);
 
 
-// Test 1 ----- Test Socket_Error exceptions
-void test_socket(int socket)
+// Test 1 ----- Test Socket_Error exception in creating
+void test_socket_creation(int const socket)
 {
     // exception handling
     if(socket == 1)
     {
         throw Socket_Error("Socket creation failed!!!");
+    }
+
+    // exit
+    return;
+}
+
+
+// Test 2 ----- Test Socket_Error exception in binding
+void test_socket_bind(int const bind, int const port)
+{
+    // exception handling
+    if(bind < 0)
+    {
+        throw Socket_Error("Binding to port " + std::to_string(port) + " failed!!!");
+    }
+
+    // exit
+    return;
+}
+
+
+// Test 3 ----- Test Socket_Error exception in listening
+void test_socket_listen(int const listen)
+{
+    // exception handling
+    if(listen < 0)
+    {
+        throw Socket_Error("Listening failed!!!");
+    }
+
+    // exit
+    return;
+}
+
+
+// Test 4 ----- Test Socket_Error exception in accepting
+void test_socket_accept(int const socket)
+{
+    // exception handling
+    if(socket < 0)
+    {
+        throw Socket_Error("The client connection has not been accepted!!!");
     }
 
     // exit
