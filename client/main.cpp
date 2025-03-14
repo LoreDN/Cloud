4f93a36f855553abfd0c5b60ca10e9d61323a46b
Lorenzo Di Napoli <loridinapoli@gmail.com>
2025-03-14 14:26:50 +0100

    Exchange message with the server

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 
 // costants
@@ -19,6 +20,7 @@ int main() {
 
     // message declaration
     char buffer[STREAM_MAX] = {0};
+    int message_length;
 
     // create a socket (IPv4 / TCP)
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -28,7 +30,7 @@ int main() {
         return -1;
     }
 
-    // connecting to the server
+    // identify the server
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0)
@@ -37,12 +39,23 @@ int main() {
         return -1;
     }
 
-    
+    // connect to the server
     if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
     {
         std::cerr << "Connection failed!!!" << std::endl;
         return -1;
     }
+
+    // send and receive message
+    send(client_socket, "Hello, Server!", 15, 0);
+    message_length = read(client_socket, buffer, sizeof(buffer));
+    if (message_length > 0)
+    {     
+        std::cout << "Message from server: " << buffer << std::endl;
+    }
+
+    // close the socket
+    close(client_socket);
 
     // exit
     return 0;
